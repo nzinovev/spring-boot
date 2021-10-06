dd9ccbb03373c5dd620780a1e06530fd997124e3
nzinovev <nervous9339@gmail.com>
2021-10-07 01:47:31 +0300

    spotless

--- src/main/java/com/example/springboot/repository/UserRepository.java
+++ src/main/java/com/example/springboot/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findUserByEmail(String email);
+	User findUserByEmail(String email);
 }
