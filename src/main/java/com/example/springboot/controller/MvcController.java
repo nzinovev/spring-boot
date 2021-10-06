f11f69108f89da095f6dbadb35454292d8105084
nzinovev <nervous9339@gmail.com>
2021-10-06 20:16:05 +0300

    validation added

--- src/main/java/com/example/springboot/controller/MvcController.java
+++ src/main/java/com/example/springboot/controller/MvcController.java
@@ -5,9 +5,12 @@ import com.example.springboot.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,12 +27,19 @@ public class MvcController {
     public String users(Model model) {
         var dtoList = userService.findAll();
         model.addAttribute("users", dtoList);
-        model.addAttribute("user", new UserDto());
-        return "user";
+        return "users";
+    }
+
+    @GetMapping("/sign-up")
+    public String login(@ModelAttribute(name = "user") UserDto user) {
+        return "add-user";
     }
 
     @PostMapping("/users")
-    public String createUser(UserDto user, Model model) {
+    public String createUser(@Valid @ModelAttribute(name = "user") UserDto user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
         userService.createUser(user);
 
         return "redirect:/users";
--- src/main/java/com/example/springboot/domain/dto/UserDto.java
+++ src/main/java/com/example/springboot/domain/dto/UserDto.java
@@ -2,10 +2,20 @@ package com.example.springboot.domain.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserDto {
 
+    @Email
+    @NotBlank
     String email;
+    @NotBlank
+    @Size(min = 5, max = 15)
     String password;
+    @NotBlank
+    @Size(min = 5, max = 15)
     String confirmPassword;
 }
